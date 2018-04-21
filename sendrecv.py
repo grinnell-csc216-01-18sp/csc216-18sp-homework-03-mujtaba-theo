01de52bac94cc54cf64e8ad38581aa56d7bba5f9
Theo Kalfas <kalfastheodoros@gmail.com>
2018-04-21 17:55:30 -0500

    Added GBN implementation

--- sendrecv.py
+++ sendrecv.py
@@ -34,10 +34,11 @@ from sendrecvbase import BaseSender, BaseReceiver
 import Queue
 
 class Segment:
-    def __init__(self, msg, dst, bit=False):
+    def __init__(self, msg, dst, bit=False, seqnum=0):
         self.msg = msg
         self.dst = dst
         self.bit = bit
+        self.seqnum = seqnum
 
     def is_corrupt(self):
         return self.msg == '<CORRUPTED>'
@@ -69,22 +70,22 @@ class AltSender(BaseSender):
         self.bit = False
         self.wait = False
 
+    def is_valid_ack(self, seg):
+        return seg.msg == '<ACK>' and seg.bit == self.bit
+
     def receive_from_app(self, msg):
         # print('received message {} from app'.format(msg))
         if self.wait == True:
             # print('but waiting, so dropping it')
             return
         self.curr_msg = msg
-        seg = Segment(msg, 'receiver', self.bit)
+        seg = Segment(msg, 'receiver', bit=self.bit)
         # print('sending to network')
         self.send_to_network(seg)
         self.wait = True
 
         self.timer_bit = self.bit
         self.start_timer(5)
-
-    def is_valid_ack(self, seg):
-        return seg.msg == '<ACK>' and seg.bit == self.bit
 
     def receive_from_network(self, seg):
         # print('received {} from network'.format(seg.msg))
@@ -95,7 +96,7 @@ class AltSender(BaseSender):
         else:
             # print('but its corrupt or not ACK or different bit, so resending')
             # print('sender bit: {}, ACK bit: {}, are we in wait? {}'.format(self.bit, seg.bit, self.wait))
-            seg = Segment(self.curr_msg, 'receiver', self.bit)
+            seg = Segment(self.curr_msg, 'receiver', bit=self.bit)
             self.send_to_network(seg)
 
             self.timer_bit = self.bit
@@ -105,7 +106,7 @@ class AltSender(BaseSender):
         if self.timer_bit == self.bit:
             self.start_timer(5)
             # print('Times up and bit hasnt changed, resending')
-            seg = Segment(self.curr_msg, 'receiver', self.bit)
+            seg = Segment(self.curr_msg, 'receiver', bit=self.bit)
             self.send_to_network(seg)
             return
         # print('Times up and bit has changed, moving on')
@@ -119,20 +120,80 @@ class AltReceiver(BaseReceiver):
         # print('received message {} from client'.format(seg.msg))
         if seg.is_corrupt():
             # print('but is corrupt, so sending NAK')
-            self.send_to_network(Segment('<NAK>', 'sender', self.bit))
+            self.send_to_network(Segment('<NAK>', 'sender', bit=self.bit))
         elif seg.bit != self.bit:
             # print('but is wrong bit, so resending ACK')
-            self.send_to_network(Segment('<ACK>', 'sender', not self.bit))
+            self.send_to_network(Segment('<ACK>', 'sender', bit=not self.bit))
         else:
             # print('and its ok, so sending to app, sending ACK and switching bit')
             self.send_to_app(seg.msg)
-            self.send_to_network(Segment('<ACK>', 'sender', self.bit))
+            self.send_to_network(Segment('<ACK>', 'sender', bit=self.bit))
             self.bit = not self.bit
 
 class GBNSender(BaseSender):
-    # TODO: fill me in!
-    pass
+    def __init__(self, app_interval, base=1, nextseq=1, N=5):
+        super(GBNSender, self).__init__(app_interval)
+        self.base = base
+        self.nextseq = nextseq
+        self.messages = {}
+        self.timer = False
+        self.start_timer(5)
+        self.N = N
+
+    def receive_from_app(self, msg):
+        print('got message {} from app'.format(msg))
+        if self.nextseq >= self.base + self.N:
+            print('too many packages ({} >= {} + {}), returning'.format(self.nextseq, self.base, self.N))
+            return
+        self.messages[self.nextseq] = msg
+        seg = Segment(msg, 'receiver', seqnum=self.nextseq)
+        self.send_to_network(seg)
+        if self.base == self.nextseq:
+            self.timer = True
+        self.nextseq = self.nextseq + 1
+
+    def receive_from_network(self, seg):
+        print('______RECEIVED FROM NETWORK, ({}, {})'.format(seg.msg, seg.seqnum))
+        if seg.is_corrupt() or seg.msg != '<ACK>':
+            print('but its corrupt or not ack')
+            return
+        self.base = seg.seqnum + 1
+        if self.base == self.nextseq:
+            self.timer = False
+        else:
+            self.timer = True
+
+    def on_interrupt(self):
+        print('timer running now....')
+        self.start_timer(5)
+        if self.timer == False:
+            print('lol no, timer is set to false')
+            return
+        print('resending packets')
+        for i in range(self.base, self.nextseq):
+            print('resending packet {}'.format(i))
+            msg = self.messages[i]
+            seg = Segment(msg, 'receiver', seqnum=i)
+            self.send_to_network(seg)
+
+
 
 class GBNReceiver(BaseReceiver):
-    # TODO: fill me in!
-    pass
+    def __init__(self, expected=1):
+        super(GBNReceiver, self).__init__()
+        self.expected = expected
+        self.last_ack_num = 0
+
+    def receive_from_client(self, seg):
+        print('received ({}, {}) from client'.format(seg.msg, seg.seqnum))
+        if (not seg.is_corrupt()) and self.expected == seg.seqnum:
+            print('and its fine, so sending to app, updating last ACK')
+            self.send_to_app(seg.msg)
+            last_ack = Segment('<ACK>', 'sender', seqnum=self.expected)
+            self.send_to_network(last_ack)
+            self.expected = self.expected + 1
+            self.last_ack_num = self.last_ack_num + 1
+        else:
+            print('its bad ({} vs {}), resending last ack'.format(self.expected, seg.seqnum))
+            last_ack = Segment('<ACK>', 'sender', seqnum=self.last_ack_num)
+            self.send_to_network(last_ack)
